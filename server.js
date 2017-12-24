fdd17d44c5e1beeef6e9bf30a8499915d49014f2
briandavidjoyner <brianjoyner@brians-mbp.attlocal.net>
2017-12-24 18:16:19 -0500

    Start

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var email = require('./API/email/mailchimp.js');
 var bodyParser = require('body-parser');
 var sm = require('sitemap');
 
+
 //OpenShift Settings
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
